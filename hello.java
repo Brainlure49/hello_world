ba9ca936a987a70037c0caaac1b92af036002fc3
Brainlure49 <adamsbe@my.easternct.edu>
2017-10-23 16:59:36 -0400

    Edits
    Added a scanner object, added another text output asking for the user's name, and changed the hello world string to include my name and the name the user entered

--- hello.java
+++ hello.java
@@ -4,10 +4,16 @@ public class hello {
 
     public static void main(String [] args) {
 
-
-	// added  line	
-
-        System.out.println("Hello Class!");
+		
+		
+		String myName = "Ben";
+		
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Please enter your name: ");
+		String userName = reader.nextString();
+		reader.close();
+        System.out.println("Hello, " + userName + ", my name is " + myName + "!";);
 
 
 
